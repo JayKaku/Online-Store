8e8f36c3bfc86e3198c47476d4b9791c5acfb26a
Jay Kaku <61429929+JayKaku@users.noreply.github.com>
2020-08-02 22:13:49 +0530

    Update fbConfig.js

--- src/config/fbConfig.js
+++ src/config/fbConfig.js
@@ -3,14 +3,14 @@ import "firebase/firestore";
 
 // Your web app's Firebase configuration
 var firebaseConfig = {
-  apiKey: "AIzaSyAhZ1_lGQ4rO8jrKG_evZUWYOadLBw4jU4",
-  authDomain: "jay-mymarket.firebaseapp.com",
-  databaseURL: "https://jay-mymarket.firebaseio.com",
-  projectId: "jay-mymarket",
-  storageBucket: "jay-mymarket.appspot.com",
-  messagingSenderId: "531493143194",
-  appId: "1:531493143194:web:9137e09326ae85a6595523",
-  measurementId: "G-S2JKLMNQC3",
+  apiKey: "****************",
+  authDomain: "*************",
+  databaseURL: "*************",
+  projectId: "*********",
+  storageBucket: "************",
+  messagingSenderId: "*********",
+  appId: "*************",
+  measurementId: "***********",
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
